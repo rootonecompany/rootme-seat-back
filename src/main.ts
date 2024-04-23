0fe3476b261faa2982dd154c692d019bf61f6e3a
jarvis1543 <pjy4952@naver.com>
2024-04-23 20:13:01 +0900

    change port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ConfigService } from "@nestjs/config";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
-    const port = configService.get<number>("PORT", 3000);
+    const port = configService.get<number>("PORT", 8080);
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
