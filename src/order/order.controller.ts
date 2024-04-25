f951e0f6507bf86872e759c86dd26fbe87552254
jarvis1543 <pjy4952@naver.com>
2024-04-25 11:24:11 +0900

    update: update @ApiTags, @ApiOperation

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 import { OrderService } from "./order.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 export type OrderQueryType = {
     orderNum: string;
@@ -14,15 +15,25 @@ export type MyOrderBodyType = OrderQueryType & {
 };
 
 @Controller("order")
+@ApiTags("주문 API")
 export class OrderController {
     constructor(private readonly orderService: OrderService) {}
 
     @Post()
+    @ApiOperation({
+        summary: "주문 확인",
+        description:
+            "1) 주문번호가 없으면 insert \n2) 주문번호가 있고, 좌석이 없으면 {}\n3) 주문번호가 있고, 좌석이 있으면 invoice값 호출",
+    })
     public async isOrder(@Body() body: MyOrderBodyType) {
         return await this.orderService.isOrder(body);
     }
 
     @Get("/invoice")
+    @ApiOperation({
+        summary: "인보이스",
+        description: "주문번호로 예매된 모든 값 호출",
+    })
     public async getMyOrder(@Query() query: OrderQueryType) {
         return await this.orderService.isOrder(query);
     }
--- src/theater/theater.controller.ts
+++ src/theater/theater.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Put, Query } from "@nestjs/common";
 import { TheaterService } from "./theater.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 export type InsertTheaterBodyType = {
     theaterCode: string;
@@ -50,50 +51,87 @@ export type dateQueryType = {
 };
 
 @Controller("theater")
+@ApiTags("소극장 관련 API")
 export class TheaterController {
     constructor(private readonly theaterService: TheaterService) {}
 
     @Post("/insertTheaterInfos")
+    @ApiOperation({
+        summary: "소극장 정보 저장",
+        description: "소극장 정보 저장",
+    })
     public async insertTheaterInfos(@Body() body: InsertTheaterBodyType) {
         return await this.theaterService.insertTheaterInfos(body);
     }
 
     @Post("/insertCustomDates")
+    @ApiOperation({
+        summary: "소극장의 예매할 수 있는 날짜 저장",
+        description: "소극장의 예매할 수 있는 날짜 저장",
+    })
     public async insertCustomDates(@Body() body: InsertDateBodyType) {
         return await this.theaterService.insertCustomDates(body);
     }
 
     @Post("/insertCustomTimes")
+    @ApiOperation({
+        summary: "소극장의 예매할 수 있는 시간 저장",
+        description: "소극장의 예매할 수 있는 시간 저장",
+    })
     async insertCustomTimes(@Body() body: InsertTimeBodyType) {
         return await this.theaterService.insertCustomTimes(body);
     }
 
     @Post("/insertCustomRows")
+    @ApiOperation({
+        summary: "소극장의 예매할 수 있는 행 저장",
+        description: "소극장의 예매할 수 있는 행 저장",
+    })
     async insertCustomRows(@Body() body: InsertRowBodyType) {
         return await this.theaterService.insertCustomRows(body);
     }
 
     @Post("/insertCustomSeats")
+    @ApiOperation({
+        summary: "소극장의 예매할 수 있는 좌석 저장",
+        description: "소극장의 예매할 수 있는 좌석 저장",
+    })
     async insertCustomSeats(@Body() body: InsertSeatBodyType) {
         return await this.theaterService.insertCustomSeats(body);
     }
 
     @Get("/dates")
+    @ApiOperation({
+        summary: "소극장의 예매할 수 있는 날짜 호출",
+        description: "소극장의 예매할 수 있는 날짜 호출",
+    })
     async getDates(@Query() query: TheaterQueryType) {
         return await this.theaterService.getDates(query);
     }
 
     @Get("/times")
+    @ApiOperation({
+        summary: "소극장의 예매할 수 있는 시간 호출",
+        description: "소극장의 예매할 수 있는 시간 호출",
+    })
     async getTimes(@Query() query: DateQueryType) {
         return await this.theaterService.getTimes(query);
     }
 
     @Get("/seats")
+    @ApiOperation({
+        summary: "소극장의 예매할 수 있는 좌석 호출",
+        description: "소극장의 예매할 수 있는 좌석 호출",
+    })
     async getSeats(@Query() query: TimeQueryType) {
         return await this.theaterService.getSeats(query);
     }
 
     @Put("/seats")
+    @ApiOperation({
+        summary: "좌석 예매",
+        description: "좌석 예매",
+    })
     async updateSeats(@Body() body: UpdateSeatBodyType) {
         return await this.theaterService.updateSeats(body);
     }
