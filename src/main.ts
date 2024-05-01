70dbbac77a2e8d637802d2f14bf195b3e8fff75e
jarvis1543 <pjy4952@naver.com>
2024-05-01 18:54:26 +0900

    update: main.ts add cors: true

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from "@nestjs/config";
 import { initSwagger } from "./config/swagger";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     const configService = app.get(ConfigService);
     const port = configService.get<number>("PORT", 7989);
     app.useGlobalPipes(
