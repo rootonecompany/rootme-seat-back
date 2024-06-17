8c2de58d26967e351f2278c8b3fef8f822a1380b
jarvis1543 <pjy4952@naver.com>
2024-06-17 18:07:04 +0900

    feat: [add] 주문자정보 엔티티 추가

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -4,9 +4,10 @@ import { OrderController } from "./order.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Order } from "src/yanolja/entities/order.entity";
 import { Theater } from "src/yanolja/entities/theater.entity";
+import { OrderInfo } from "src/yanolja/entities/orderInfo.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Order, Theater])],
+    imports: [TypeOrmModule.forFeature([Order, Theater, OrderInfo])],
     providers: [OrderService],
     controllers: [OrderController],
 })
